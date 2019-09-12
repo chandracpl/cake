7de9b54fb160d56047e9e9c2c6a406bd05f33146
Ubuntu <you@example.com>
2019-09-12 11:00:56 +0000

    cake

---
+++ src/main/java/com/cake/App.java
@@ -0,0 +1,13 @@
+package com.cake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
